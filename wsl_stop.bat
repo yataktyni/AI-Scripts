c605002c37b99a2277b82cd92fd8f591b2d5a39d
Ya Tak — Ty Ni <77163051+yataktyni@users.noreply.github.com>
2025-04-10 10:59:38 +0300

    Update wsl_stop.bat

--- wsl_stop.bat
+++ wsl_stop.bat
@@ -3,7 +3,7 @@ SETLOCAL
 
 :: ===========================================
 :: Script Name: wsl_stop.bat
-:: Description: This script cleans up and compacts your WSL2 Ubuntu instance.
+:: Description: This script stops your WSL2 Ubuntu instance, and for related service to disabled state to prevent it from autorun without calling it again.
 :: Author: yataktyni (https://x.com/yataktyni)
 :: License: MIT License
 :: Copyright: (c) 2025 yataktyni. All Rights Reserved.
